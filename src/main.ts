87dc52acd7ff5fb03e74895e3d477db45bf878cc
kometenstaub <83140328+kometenstaub@users.noreply.github.com>
2021-09-23 22:08:12 +0200

    change names, function to collect heading names

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,43 @@
-import { App, FileSystemAdapter, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	App,
+	FileSystemAdapter,
+	Modal,
+	Notice,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+} from 'obsidian';
 import { parse } from 'ndjson';
 import { createReadStream, ReadStream } from 'fs';
 
-interface LCSHHeadingsSettings {
+interface LCSHSettings {
 	mySetting: string;
 }
 
-const DEFAULT_SETTINGS: LCSHHeadingsSettings = {
-	mySetting: 'default'
-}
+const DEFAULT_SETTINGS: LCSHSettings = {
+	mySetting: 'default',
+};
 
+export default class LCSHPlugin extends Plugin {
+	settings: LCSHSettings;
 
-export default class LCSHHeadingsPlugin extends Plugin {
-	settings: LCSHHeadingsSettings;
-
-	loadHeadingsData () : void{
-		const path = this.getAbsolutePath('lcsh.both.ndjson')
+	loadHeadingsData(): void {
+		const path = this.getAbsolutePath('lcsh.skos.ndjson');
+		let headings = new Set();
 		createReadStream(path)
 			.pipe(parse())
-			.on('data', function(obj){
-			console.log(Object.keys(obj))
-		});
+			.on('data', function (obj) {
+				obj['@graph'].map((element: { [x: string]: string }) => {
+					if (element['@id'].includes('id.worldcat.org')) {
+						headings.add(
+							element[
+								'http://www.loc.gov/mads/rdf/v1#authoritativeLabel'
+							]
+						);
+					}
+				});
+			});
+		console.log(headings);
 	}
 
 	getAbsolutePath(fileName: string): string {
@@ -35,7 +52,7 @@ export default class LCSHHeadingsPlugin extends Plugin {
 			throw new Error('Cannot determine base path.');
 		}
 		// relative path
-		relativePath = `${this.app.vault.configDir}/plugins/obsidian-lcsh-headings/${fileName}`;
+		relativePath = `${this.app.vault.configDir}/plugins/obsidian-lcsh/${fileName}`;
 		// absolute path
 		return `${basePath}/${relativePath}`;
 	}
@@ -46,24 +63,26 @@ export default class LCSHHeadingsPlugin extends Plugin {
 		await this.loadSettings();
 
 		this.addCommand({
-			id: 'read-lcsh-headings-data',
+			id: 'read-lcsh-data',
 			name: 'Read and log headings data',
 			callback: () => {
 				this.loadHeadingsData();
 			},
-			
 		});
 
-		this.addSettingTab(new LCSHHeadingsSettingTab(this.app, this));
-
+		this.addSettingTab(new LCSHSettingTab(this.app, this));
 	}
 
 	onunload() {
-		console.log('unloading LCSH plugin')
+		console.log('unloading LCSH plugin');
 	}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 
 	async saveSettings() {
@@ -71,47 +90,49 @@ export default class LCSHHeadingsPlugin extends Plugin {
 	}
 }
 
-class LCSHHeadingsModal extends Modal {
+class LCSHModal extends Modal {
 	constructor(app: App) {
 		super(app);
 	}
 
 	onOpen() {
-		let {contentEl} = this;
+		let { contentEl } = this;
 		contentEl.setText('Woah!');
 	}
 
 	onClose() {
-		let {contentEl} = this;
+		let { contentEl } = this;
 		contentEl.empty();
 	}
 }
 
-class LCSHHeadingsSettingTab extends PluginSettingTab {
-	plugin: LCSHHeadingsPlugin;
+class LCSHSettingTab extends PluginSettingTab {
+	plugin: LCSHPlugin;
 
-	constructor(app: App, plugin: LCSHHeadingsPlugin) {
+	constructor(app: App, plugin: LCSHPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		let {containerEl} = this;
+		let { containerEl } = this;
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'LCSH Headings plugin settings.'});
+		containerEl.createEl('h2', { text: 'LCSH Headings plugin settings.' });
 
 		new Setting(containerEl)
 			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue('')
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setDesc("It's a secret")
+			.addText((text) =>
+				text
+					.setPlaceholder('Enter your secret')
+					.setValue('')
+					.onChange(async (value) => {
+						console.log('Secret: ' + value);
+						this.plugin.settings.mySetting = value;
+						await this.plugin.saveSettings();
+					})
+			);
 	}
 }
