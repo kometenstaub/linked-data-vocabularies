258a90ee6d5144d4a5607b38ca1e3ab1ca5bf2d5
kometenstaub <83140328+kometenstaub@users.noreply.github.com>
2021-09-25 20:59:56 +0200

    await, but still Error, doesn't log in renderSuggestion

--- src/suggester.ts
+++ src/suggester.ts
@@ -1,40 +1,46 @@
 import { App, SuggestModal } from 'obsidian';
 import type SKOSPlugin from './main';
 
+
+
+
 export interface SuggesterItem {
 	display: string; // the heading that is displayed to the user
 	url: string; // the URL for getting the necessary data
 }
 
-export class SKOSFuzzyModal extends SuggestModal<SuggesterItem> {
+export class SKOSFuzzyModal extends SuggestModal<Promise<SuggesterItem>> {
 
 	constructor(app: App, public plugin: SKOSPlugin) {
 		super(app);
 	}	
-
 	
-
-	getSuggestions(query: string): SuggesterItem[] {
+//	Property 'getSuggestions' in type 'SKOSFuzzyModal' is not assignable to the same property in base type 'SuggestModal<Promise<SuggesterItem>>'.
+//  Type '(query: string) => Promise<SuggesterItem[]>' is not assignable to type '(query: string) => Promise<SuggesterItem>[]'.
+//    Type 'Promise<SuggesterItem[]>' is missing the following properties from type 'Promise<SuggesterItem>[]': length, pop, push, concat, and 31 more.ts(2416)
+	async getSuggestions(query: string): Promise<SuggesterItem[]> {
 		const input = this.inputEl.value
 		let output : SuggesterItem[] = []
-		this.plugin.methods.findHeading(input).then( (success) => {
-			output = success
-		})
+		output = await this.plugin.methods.findHeading(input)
+		// works until here
+		console.log(output)
 		return output
+		
+
 	}
-	renderSuggestion(value: SuggesterItem, el: HTMLElement) {
+	async renderSuggestion(value: Promise<SuggesterItem>, el: HTMLElement) {
 		// this doesn't get logged
-		console.log(value.display)
-		el.createEl('div', value.display)
+		const newValue = await value
+		console.log(newValue.display)
+		el.createEl('div', newValue.display)
 		
 	}
 	// this can't be called because no suggestions are rendered
-	onChooseSuggestion(item: SuggesterItem, evt: MouseEvent | KeyboardEvent) {
-		this.plugin.methods.getURL(item).then( (success) => {
-			this.plugin.methods.parseSKOS(success).then((success) => {
-				console.log('success')
-			})
-		})
+	async onChooseSuggestion(item: Promise<SuggesterItem>, evt: MouseEvent | KeyboardEvent) {
+		const newItem = await item
+		const url = await this.plugin.methods.getURL(newItem)
+		this.plugin.methods.parseSKOS(url)
+		
 		
 	}
 
